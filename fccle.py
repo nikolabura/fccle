55a67f0f29653811fc94e20cd77c76956af657a0
Nikola Bura <nbura1@umbc.edu>
2021-05-20 16:40:20 +0000

    edits

--- fccle.py
+++ fccle.py
@@ -15,7 +15,7 @@ def root():
 def microwave_towers():
     lat = request.args.get("lat")
     lon = request.args.get("lon")
-    sql = f"""
+    sql = """
     SELECT jsonb_build_object(
         'type',     'FeatureCollection',
         'features', jsonb_agg(features.feature)
@@ -32,8 +32,8 @@ def microwave_towers():
             FROM fccle."dd_micro_LO"
             WHERE ST_DWithin(geom,
                              ST_SetSRID(ST_MakePoint(%s,%s), 4326),
-                             0.06)
-            LIMIT 500
+                             0.09)
+            LIMIT 700
         ) inputs
     ) features;
     """
@@ -43,3 +43,60 @@ def microwave_towers():
     cur.close()
     return jsonify(towers)
 
+@app.route("/api/microwave_paths")
+def microwave_paths():
+    iterations = request.args.get("iterations")
+    callsign = request.args.get("callsign")
+    location_number = request.args.get("loc_number")
+    cur = conn.cursor()
+    sql = """
+    SELECT * FROM fccle.microwave_paths m
+    WHERE
+        (m.callsign = %s AND m.transmit_location_number = %s)
+        OR (m.receiver_callsign = %s AND m.receiver_location_number = %s)
+    LIMIT 50;
+    """
+    cur.execute(sql, (callsign, location_number, callsign, location_number))
+    paths = cur.fetchall()
+    partners = []
+    me = (callsign, int(location_number))
+    for path in paths:
+        #print(path)
+        if path[4] == None or path[16] == None:
+            print("Had none")
+            continue
+        tx_callsign = path[4].strip()
+        tx_locnum = path[7]
+        rx_callsign = path[16].strip()
+        rx_locnum = path[9]
+        tx = (tx_callsign, tx_locnum)
+        rx = (rx_callsign, rx_locnum)
+        if tx != me:
+            partners.append(tx)
+        if rx != me:
+            partners.append(rx)
+    partners = set(partners)
+    print(partners)
+    if len(partners) == 0:
+        return "[]"
+    partners_json_out = []
+    for partner in partners:
+        sql = """
+        SELECT jsonb_build_object(
+            'type',       'Feature',
+            'id',         id,
+            'geometry',   ST_AsGeoJSON(geom)::jsonb,
+            'properties', to_jsonb(inputs) - 'gid' - 'geom'
+        ) AS feature
+        FROM (
+            SELECT *
+            FROM fccle."dd_micro_LO"
+            WHERE call_sign = %s AND location_number = %s
+            LIMIT 500
+        ) inputs;
+        """
+        cur.execute(sql, (partner[0], partner[1]))
+        partner_geojson = cur.fetchone()[0]
+        partners_json_out.append(partner_geojson)
+    cur.close()
+    return jsonify(partners_json_out)
--- static/fccle.js
+++ static/fccle.js
@@ -14,22 +14,114 @@ const CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_al
   maxZoom: 19
 });
 
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var wmsLayer = L.tileLayer.wms('http://fccle.greenbank.lan:8080/geoserver/dd_micro_LO/wms?', {
+    layers: "dd_micro_LO"
+})
+
 CartoDB_Positron.addTo(map);
 
-/*var wmsLayer = L.tileLayer.wms('http://fccle.greenbank.lan:8080/geoserver/dd_micro_LO/wms?', {
-    layers: "dd_micro_LO"
-}).addTo(map);*/
+let basemaps = {
+  "Positron": CartoDB_Positron,
+  "Terrain": Stamen_Terrain,
+  "Satellite": Esri_WorldImagery,
+  "Microwave Tower Dots": wmsLayer
+};
+L.control.layers(basemaps).addTo(map);
+
+
+function log(str) {
+  const textarea = document.getElementById("log");
+  textarea.innerHTML += "\n" + str;
+  textarea.scrollTop = textarea.scrollHeight;
+}
+
+let paths = [];
+let paths_layer = L.polyline(paths, { color: 'red', weight: 2 }).addTo(map);
+
+function pathTrace(callsign, loc_number, iterations, my_long, my_lat, prev_ids) {
+  fetch(`/api/microwave_paths?callsign=${callsign}&loc_number=${loc_number}&iterations=${iterations}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      document.getElementById("dash").innerHTML += "\n" + JSON.stringify(data);
+      document.getElementById("dash").scrollTop = document.getElementById("dash").scrollHeight;
+      const existing_ids = microwave_towers.toGeoJSON().features.map((x) => x.id);
+      for (feature of data) {
+        if (!existing_ids.includes(feature.id)) {
+          log(`New tower discovered! Callsign ${feature.properties.call_sign}.`)
+          microwave_towers.addData(feature);
+        }
+        const fc = feature.geometry.coordinates;
+        paths.push([[my_lat, my_long], [fc[1], fc[0]]]);
+        paths_layer.remove();
+        paths_layer = L.polyline(paths, { color: 'red' }).addTo(map);
+        const callsign = feature.properties.call_sign;
+        const loc_number = feature.properties.location_number;
+        const my_id = feature.id;
+        if (!prev_ids.includes(my_id)) {
+          let new_arr = prev_ids.slice();
+          new_arr.push(my_id)
+          pathTrace(callsign, loc_number, 1, fc[0], fc[1], new_arr);
+        }
+      }
+      microwave_markers.addLayer(microwave_towers);
+      map.addLayer(microwave_markers);
+    });
+}
 
 function onEachMicrowaveTower(feature, layer) {
-  layer.bindPopup("Call sign: <b>" + feature.properties.call_sign + "</b>");
+  const callsign = feature.properties.call_sign;
+  const loc_number = feature.properties.location_number;
+  const my_id = feature.id;
+  layer.bindPopup("Call sign: <b>" + feature.properties.call_sign + "</b><br>"
+    + "Ground elevation: <b>" + feature.properties.ground_elevation + "</b><br>"
+    + `Path trace: <button onclick="pathTrace('${callsign}', ${loc_number}, 1, ${feature.geometry.coordinates}, [${my_id}])">1</button>`);
   /*layer.on("click", (e) => {
-
   });*/
 }
 
-const microwave_towers = L.geoJSON(null, {
+const microwave_markers = L.markerClusterGroup();
+let awaiting_mass_click = false;
+function analyzeCluster() {
+  if (awaiting_mass_click) {
+    awaiting_mass_click = false;
+    log("Cluster analysis deactivated.");
+  } else {
+    awaiting_mass_click = true;
+    log("Cluster analysis on, awaiting click on cluster...");
+  }
+}
+microwave_markers.on("clusterclick", (e) => {
+  if (!awaiting_mass_click) return;
+  console.log(e);
+  log("Click received.");
+  e.originalEvent.preventDefault();
+  awaiting_mass_click = false;
+  markers = e.layer.getAllChildMarkers();
+  if (markers.length > 100) {
+    const confout = confirm(`Warning: This cluster contains ${markers.length} microwave towers.`
+      + ` Are you sure you want to path-trace them all? There will be a large performance hit and this may take a while.`);
+    if (!confout) return;
+  }
+  for (marker of markers) {
+    const feature = marker.feature;
+    const callsign = feature.properties.call_sign;
+    const loc_number = feature.properties.location_number;
+    console.log(feature.geometry.coordinates);
+    const my_id = feature.id;
+    pathTrace(callsign, loc_number, 1, feature.geometry.coordinates[0], feature.geometry.coordinates[1], [my_id]);
+  }
+})
+
+let microwave_towers = L.geoJSON(null, {
   onEachFeature: onEachMicrowaveTower
-}).addTo(map);
+});
+microwave_markers.addLayer(microwave_towers);
+map.addLayer(microwave_markers);
 
 function loadMicrowave() {
   const center = map.getCenter();
@@ -38,10 +130,26 @@ function loadMicrowave() {
     .then(data => {
       data = data[0];
       const existing_ids = microwave_towers.toGeoJSON().features.map((x) => x.id);
+      let addedCount = 0;
       for (feature of data.features) {
         if (!existing_ids.includes(feature.id)) {
           microwave_towers.addData(feature);
+          addedCount++;
         }
       }
+      microwave_markers.clearLayers();
+      microwave_markers.addLayer(microwave_towers);
+      map.addLayer(microwave_markers);
+      log(`Database returned ${data.features.length} towers. ${addedCount} were new.`);
     });
+}
+
+function clearTowers() {
+  microwave_towers.clearLayers();
+  microwave_markers.clearLayers();
+}
+
+function clearPaths() {
+  paths = [];
+  paths_layer.remove();
 }
