767c5880482a722ca7165434995fbc2d183eaad2
Nikola Bura <nbura1@umbc.edu>
2021-05-22 03:17:23 +0000

    edit script

--- utils/radio_script.sh
+++ utils/radio_script.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-echo $@ > /home/nikola/umbc/2021a-spring/ges486/final/sandbox.txt
 ARG=$@
 ARGM=$(cut -d "/" -f2- <<< "$ARG")
 ARGD=$(cut -d "/" -f2- <<< "$ARGM")
-echo $ARGD > /home/nikola/umbc/2021a-spring/ges486/final/sandbox.txt
 a=$(bc<<<1000*$ARGD)
-echo $a >> /home/nikola/umbc/2021a-spring/ges486/final/sandbox.txt
 sleep 0.3
 wmctrl -a Gqrx
 sleep 0.3
